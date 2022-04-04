4065bca47f7bd60a353bdc7d25bc5cabfcb24056
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2022-04-04 17:22:25 -0300

    Exercicio

---
+++ ExerciciosLacosRepeticao/Exercicio5.java
@@ -0,0 +1,30 @@
+package ExerciciosLacosRepeticao;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+	public static void main(String[] args) {
+		/*
+		 *5 - Crie um programa que leia um número do teclado até que encontre um
+número igual a zero. No final, mostre a soma dos números
+digitados.(DO...WHILE)
+		 */
+		Scanner leia = new Scanner(System.in);
+		int num, soma = 0;
+		
+		do {
+			
+			System.out.println(" Digite um número: ");
+			num = leia.nextInt(); 
+			soma+=num;
+			
+			
+		}while (num !=0);
+		
+		System.out.println("A soma dos dois Números Digitados é: " + soma);
+	
+
+	}
+
+}
